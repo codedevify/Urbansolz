5b974d8dc1eb82229af1ace4896d3d759f3862ac
codedevify <codedevify@gmail.com>
2025-11-10 01:31:16 +0300

    Update Product.js

--- models/Product.js
+++ models/Product.js
@@ -1,10 +1,11 @@
+// models/Product.js
 const mongoose = require('mongoose');
 
 const productSchema = new mongoose.Schema({
-  name: String,
-  description: String,
-  price: Number,
-  image: String
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  price: { type: Number, required: true },
+  image: { type: String, default: '' }  // ← Prevents undefined
 });
 
 module.exports = mongoose.model('Product', productSchema);
